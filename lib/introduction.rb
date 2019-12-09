b76685a1cdeb435784d3665a2aa6a3b9728b1b75
Jason Viola <jason.viola@mavs.uta.edu>
2019-12-09 14:06:22 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hello, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+  puts "Hello, my name is #{name} and I am learning to code in #{language}."
+end
